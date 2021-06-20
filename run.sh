5d95a4203a091b7efcacc9dd1f1cd7fca468afcd
Jitendra Mishra <jitendra29mishra@gmail.com>
2021-06-21 00:40:05 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-echo "# StableMatchingAlgorithm" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/jitendra29mishra/StableMatchingAlgorithm.git
-git push -u origin main
+
